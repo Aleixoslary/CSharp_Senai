8dc0402cfdf9181c11f6d9be25ddffd72e2b8ef0
Aleixoslary <larissaaleixos@hotmail.com>
2019-09-16 12:02:13 -0300

    Adicionado a primeira estrutura condicional

---
+++ aula2/exemplo1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace exemplo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //DECLARANDO A VARIAVEL
+            double nota1, nota2, nota3, media;
+
+            Console.Write("Digite nota 1: ");
+            nota1= double.Parse(Console.ReadLine());
+
+            Console.Write("Digite nota 2: ");
+            nota2= double.Parse(Console.ReadLine());
+
+            Console.Write("Digite nota 3: ");
+            nota3= double.Parse(Console.ReadLine());
+
+            media=(nota1+nota2+nota3)/3;
+
+            if (media>=6){
+                Console.WriteLine("Aluno Aprovado!");
+            } else{
+               Console.WriteLine("Aluno Reprovado:");
+            }
+        }
+    }
+}
