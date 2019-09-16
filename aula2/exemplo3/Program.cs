add51d6354ce2f3b8633e1f3ec35d9825783e3bb
Aleixoslary <larissaaleixos@hotmail.com>
2019-09-16 14:14:02 -0300

    exemplo 3

---
+++ aula2/exemplo3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace exemplo3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Deixando declarada a variavel como falsa por padrão;
+            bool entrada =false;
+            int idade;
+            string retorno;
+
+            Console.Write("Digite sua idade");
+            idade= int.Parse(Console.ReadLine());
+
+            //Como já atribuimos anteriormente, mudamos o estado da váriavel
+            //para verdadeira caso entre nesta condição:
+            if (idade>17){
+                entrada = true;
+            }
+
+            if (entrada == true){
+                retorno="Entrada Permitida! :)";
+            } else
+            {
+                retorno="Entrada não permitida! :(";
+            }
+
+            Console.WriteLine(retorno);
+
+
+        }
+    }
+}
