3a4ebb1cca78af9681cc324b0c4a86d87bfa6a09
Aleixoslary <larissaaleixos@hotmail.com>
2019-09-19 12:11:08 -0300

    Laços de repitção

---
+++ Exercicios_aula_3/while/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Exercicios_aula_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+          {
+            // laço condicional composto
+            double nota = 0;
+            int cont = 0;
+            double acl = 0;
+            string sair = "";
+            // int alunos = 10;
+
+        // tornando nota double em string para tornar possível a comparação
+            while(sair != "sim"){
+                Console.WriteLine("Digite a nota de um aluno: ");
+                nota = double.Parse(Console.ReadLine());
+               
+                acl+= nota;
+                cont++;
+
+                Console.WriteLine("Pede para sair:");
+                sair = Console.ReadLine().ToLower();
+            }
+            
+            Console.WriteLine($"Média das notas: {acl /cont}");
+
+        }
+    }
+}
+
+//.ToLower() --- adciona a caixa baixa
