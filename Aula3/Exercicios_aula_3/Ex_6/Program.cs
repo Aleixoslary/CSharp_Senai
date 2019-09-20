79fb01c951ade68455a885256bfa847723752bd4
Aleixoslary <larissaaleixos@hotmail.com>
2019-09-20 11:05:45 -0300

    atualizando pasta

--- Aula3/Exercicios_aula_3/Ex_6/Program.cs
+++ Aula3/Exercicios_aula_3/Ex_6/Program.cs
@@ -21,18 +21,12 @@ namespace Ex_6
             // Console.WriteLine(n1);
 
             int n1=0, n2=1, n3, cont=0;
-            
-            if (n1==0) {
-                Console.WriteLine($"{n1}");
-
-            }
 
             for (cont=1; cont<20;cont++){
+                Console.WriteLine($"{n1}");
                 n3=n1;
                 n1=n2;
                 n2=n2+n3;
-
-                Console.WriteLine($"{n1}");
             }
         }
     }
