a96eeee4b2eb897ae71c2b40cec4ab77497e7b7f
Aleixoslary <larissaaleixos@hotmail.com>
2019-09-20 12:29:36 -0300

    Vetores/Arrays

---
+++ aula4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace aula4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] media = new double[10];
+
+            for(int i=0; i<=9; i++){
+                Console.WriteLine("Digite uma média: ");
+                media[i]=double.Parse(Console.ReadLine());
+
+            }
+
+             Console.WriteLine(media[3]+"\n");
+            
+
+             for(int i=0; i<media.Length; i++){
+
+                 Console.WriteLine("media "+ media[i]); 
+             }
+
+        }
+    }
+}
+  
