8e31b632f99809ac0ca02ae4aaf80e40e893ef3d
Aleixoslary <larissaaleixos@hotmail.com>
2019-09-24 12:00:23 -0300

    Métodos/Função

---
+++ aula5/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace aula5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nome = new string [5];
+            string[] origem = new string [5];
+            string[] destino = new string [5];
+            DateTime[] data_ida = new DateTime[5];
+            DateTime[] data_volta = new DateTime[5];
+            string sair="";
+
+            while (sair!="0"){
+                Console.WriteLine("\nBem vindo ao nosso Sistema\n\n          MENU\n1- Cadastrar Passagem\n2- Listar Passagens\n0- Sair");
+                 sair= Console.ReadLine();
+
+                switch(sair){
+
+                    case "1":
+                        for (int i=0; i<=4; i++){
+                            Console.Write("Entre com o nome do passageiro: ");
+                            nome[i]=Console.ReadLine();
+
+                            Console.Write("Qual a UF origem? ");
+                            origem[i]=Console.ReadLine();
+
+                            Console.Write("Qual a UF destino? ");
+                            destino[i]=Console.ReadLine();
+
+                            Console.Write("Qual a data de ida? ");
+                            data_ida[i]=DateTime.Parse(Console.ReadLine());
+
+                            Console.Write("Qual a data de volta? ");
+                            data_volta[i]=DateTime.Parse(Console.ReadLine());
+
+                            Console.WriteLine("Dados cadastrados com sucesso!!!");
+                        }
+                        
+                    break;
+
+                    case "2":
+                        for (int i=0; i<=4; i++){
+                            if (nome[i]!= "" && origem[i]!="" && destino[i]!="" && data_ida[i]!=null && data_volta[i]!=null){
+                                Console.WriteLine();
+                                Console.WriteLine($"Passagem Nº {i+1}");
+                                Console.WriteLine(nome[i]);
+                                Console.WriteLine(origem[i]);
+                                Console.WriteLine(destino[i]);
+                                Console.WriteLine(data_ida[i]);
+                                Console.WriteLine(data_volta[i]);
+                                Console.WriteLine();
+
+                            }
+
+                            //  Console.WriteLine($"{Nome: nome[i]} |Origem: {origem[i]} |Destino {destino[i]} |Data ida: {data_ida[i]} |Data {data_volta[i]}");
+                        }
+                    break;
+                    
+                    case "0":
+                        Console.Write("Obrigado por escolher nosso sitema");
+                    break;
+
+                    default:
+                         Console.Write("Opção inválida");
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ aula6/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace aula6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(BomDia("Qualquer coisa"));
+            
+            //Não precisa de Console.WriteLine, pois ela é tipo Void e não tem return; 
+            ImprimeDataHora();
+
+            //chamamos a função que calcula a média
+            double[] numeros = {2,5,2,8,9,5};
+
+            Console.WriteLine(CalculaMedia(numeros));
+           
+            Console.WriteLine(Soma(numeros));
+
+
+        }
+
+                                    //Funções são declaradas no class Program, pq o Main é uma função;
+                                    //ToUpper converte tudo para MAIÚSCULA;
+
+        /// <summary>
+        /// Função que escreve bom dia para o usuário
+        /// </summary>
+        /// <returns>"Acordei e não recomendo!"</returns>
+        static string BomDia(){
+        return "Acordei e não recomendo!";
+        }
+
+        /// <summary>
+        /// Função que mostra uma saudação de acordo com o horário atual
+        /// </summary>
+        /// <param name="saudacao">um texto qualquer (sobrecarga de método)</param>
+        /// <returns>Saudação de acordo com horário</returns>
+        static string BomDia(string saudacao){
+            int hora =DateTime.Now.Hour;
+
+            if (hora <12){
+                saudacao= "Acordei e não recomendo";
+
+            } else if (hora>=12 && hora <=18){
+                saudacao="Hora do almoço :)";
+            } else{
+                saudacao="Bora dormir";
+            }
+
+        return saudacao;
+        }
+
+        /// <summary>
+        /// retorna um número - Teste de Sobrecarga
+        /// </summary>
+        /// <param name="numero">Um número inteiro qualquer</param>
+        /// <returns>Número passado como parâmetro</returns>
+        static int BomDia(int numero){
+        return numero;
+        }
+
+        /// <summary>
+        /// Retorna Data e hora atuais
+        /// </summary>
+        static void ImprimeDataHora(){
+            Console.WriteLine(DateTime.Now.ToShortDateString());
+            Console.WriteLine(DateTime.Now.Hour.ToString());
+        }
+
+        /// <summary>
+        ///  Função que calcula a média de um vetor
+        /// </summary>
+        /// <param name="valores">Vetor com valores</param>
+        /// <returns>Média dos valores</returns>
+        static double CalculaMedia(double[] valores){
+
+            double resultado=0;
+            
+            for (int i=0; i<valores.Length; i++){
+
+                resultado=resultado+valores[i];
+            }
+
+            resultado=resultado/valores.Length;
+
+        return resultado;
+        }
+
+        static double Soma(double[] valores){
+            double resultado=0;
+            for(int i=0; i<valores.Length;i++){
+                resultado=resultado+valores[i];
+            }
+
+        return resultado;
+        }
+
+    }
+}
